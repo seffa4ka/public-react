2f32e9b037e3872fe46135d42801b10dc0a65674
seffka <th-seffka@mail.ru>
2018-11-01 11:18:14 +0100

    update

---
+++ src/pages/Main/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Wrapper, Title } from '../../common/components/Test';
+
+class Main extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Title>
+          Hello World!
+        </Title>
+      </Wrapper>
+    );
+  }
+}
+
+export default Main;
